11b4b4a5a94dc983d795be9eaf332f44d227d6c4
macielg1 <guflumaciel@gmail.com>
2023-09-16 12:56:44 -0300

    changed the react query persister from local storage to indexedDB

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -1,13 +1,17 @@
 // "use client";
 // import { useState } from "react";
-// import { get, set, del } from "idb-keyval";
+
 // import { QueryClient } from "@tanstack/react-query";
-// import { PersistedClient, Persister, PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+// import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+// import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 // export default function QueryProvider({ children }: { children: React.ReactNode }) {
 //   const [queryClient] = useState(() => new QueryClient({}));
 
-//   const persister = createIDBPersister();
+//   const persister = createSyncStoragePersister({
+//     storage: typeof window !== "undefined" ? window.localStorage : undefined,
+//     throttleTime: 100,
+//   });
 
 //   return (
 //     <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
@@ -16,38 +20,34 @@
 //   );
 // }
 
-// export function createIDBPersister(idbValidKey: IDBValidKey = "reactQuery") {
-//   return {
-//     persistClient: async (client: PersistedClient) => {
-//       set(idbValidKey, client);
-//     },
-//     restoreClient: async () => {
-//       return await get<PersistedClient>(idbValidKey);
-//     },
-//     removeClient: async () => {
-//       await del(idbValidKey);
-//     },
-//   } as Persister;
-// }
-
 "use client";
 import { useState } from "react";
-
+import { get, set, del } from "idb-keyval";
 import { QueryClient } from "@tanstack/react-query";
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { PersistedClient, Persister, PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
 
-  const persister = createSyncStoragePersister({
-    storage: typeof window !== "undefined" ? window.localStorage : undefined,
-    throttleTime: 100,
-  });
+  const persister = createIDBPersister();
 
   return (
     <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
       {children}
     </PersistQueryClientProvider>
   );
+}
+
+export function createIDBPersister(idbValidKey: IDBValidKey = "reactQuery") {
+  return {
+    persistClient: async (client: PersistedClient) => {
+      set(idbValidKey, client);
+    },
+    restoreClient: async () => {
+      return await get<PersistedClient>(idbValidKey);
+    },
+    removeClient: async () => {
+      await del(idbValidKey);
+    },
+  } as Persister;
 }
