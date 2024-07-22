9666503b2064bf4bfba4ac5a3b989b51abcfa74c
MacielG1 <guflumaciel@gmail.com>
2024-07-22 14:39:47 -0300

    add mute audio toggle to mobile only

--- components/Settings/ToggleMute.tsx
+++ components/Settings/ToggleMute.tsx
@@ -1,13 +1,17 @@
 import { useAudioToggle } from "@/providers/SettingsProvider";
 import { Switch } from "../Switch";
+import { useMediaQuery } from "usehooks-ts";
 
 export default function ToggleMute() {
   const { isAudioMuted, setIsAudioMuted } = useAudioToggle();
+  const isMobile = useMediaQuery("(max-width: 640px)");
 
   function handleToggle() {
     setIsAudioMuted((prev) => !prev);
     localStorage.setItem("isAudioMuted", JSON.stringify(!isAudioMuted));
   }
+
+  if (!isMobile) return null; // Displays only on mobile to enable video playback in the background while other media is playing
 
   return (
     <p className="flex items-center justify-center gap-x-2 pt-2 text-base">
