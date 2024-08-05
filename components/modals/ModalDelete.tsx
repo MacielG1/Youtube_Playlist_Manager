72ae368bc344b490e6b12226ccc0039f42d546eb
MacielG1 <guflumaciel@gmail.com>
2024-08-05 10:38:48 -0300

    ajusted title length

--- components/modals/ModalDelete.tsx
+++ components/modals/ModalDelete.tsx
@@ -12,7 +12,7 @@ export default function ModalDelete({ onClose, content }: ModalSettingsProps) {
       <button type="button" className="fixed left-0 top-0 z-30 h-full w-full cursor-default bg-black bg-opacity-70" onClick={onClose} />
       <>
         {/* Modal */}
-        <div className="fixed inset-0 z-50 mx-auto flex w-fit items-center justify-center">
+        <div className="fixed inset-0 z-50 mx-auto flex w-[50%] items-center justify-center">
           <div className="flex flex-col gap-2 rounded-lg border-2 border-neutral-700 bg-zinc-300 p-4 py-7 shadow-lg dark:border-neutral-700 dark:bg-black">
             {content}
           </div>
