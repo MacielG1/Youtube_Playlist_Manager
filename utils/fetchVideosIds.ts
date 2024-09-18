a550b481aad1809755a79f5cc67802ac7f28b82f
MacielG1 <guflumaciel@gmail.com>
2024-09-18 10:17:41 -0300

    update fetch new videos function

--- utils/fetchVideosIds.ts
+++ utils/fetchVideosIds.ts
@@ -3,7 +3,7 @@ import { get } from "idb-keyval";
 
 export default async function fetchVideosIds(playlistId: string, videosIdsRef?: React.MutableRefObject<string[]>, isChannel?: boolean) {
   try {
-    const [savedVideos = [], removedVideos = []] = await Promise.all([get(`pl=${playlistId}`), get(`plRemoved=${playlistId}`)]);
+    const removedVideos = (await get(`plRemoved=${playlistId}`)) || [];
 
     const res = await fetch(`/api/fetchVideosIds/${playlistId}`, {
       headers: {
@@ -14,29 +14,29 @@ export default async function fetchVideosIds(playlistId: string, videosIdsRef?: 
 
     if (!res.ok) {
       console.error("Error", res.statusText);
-      return savedVideos;
+      const savedVideos = await get(`pl=${playlistId}`);
+      return savedVideos || [];
     }
 
     let data = await res.json();
 
     if (isChannel) data.reverse(); // Reverse if it's a channel
 
-    const newVideos = data.filter((video: Playlist) => {
-      return !removedVideos.includes(video.id) && !savedVideos.some((savedVideo: Playlist) => savedVideo.id === video.id);
-    });
+    // Filter out videos that were manually removed
+    const newVideos = data.filter((video: Playlist) => !removedVideos.includes(video.id));
 
     if (videosIdsRef) {
-      videosIdsRef.current = data.map((item: Playlist) => item.id);
+      videosIdsRef.current = newVideos.map((item: Playlist) => item.id);
     }
 
-    const allVideos = [...newVideos, ...savedVideos];
-    const allVideosIds = allVideos.map((item: Playlist) => item.id);
+    const allVideosIds = newVideos.map((item: Playlist) => item.id);
 
     localStorage.setItem(`plVideos=${playlistId}`, JSON.stringify({ videosIds: allVideosIds, updatedTime: Date.now() }));
 
-    return allVideos;
+    return newVideos;
   } catch (error) {
     console.error("Error fetching data in fetchVideosIds:", error);
-    return (await get(`pl=${playlistId}`)) || [];
+    const savedVideos = await get(`pl=${playlistId}`);
+    return savedVideos || [];
   }
 }
